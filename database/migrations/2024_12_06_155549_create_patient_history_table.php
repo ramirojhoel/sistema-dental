63fd2eb9670356c7dacc7563203c50e98391b354
ramirojhoel <ramirojhoel1998@gmail.com>
2024-12-06 15:00:18 -0400

    tabla actualizada paciente historial

--- database/migrations/2024_12_06_155549_create_patient_history_table.php
+++ database/migrations/2024_12_06_155549_create_patient_history_table.php
@@ -13,8 +13,6 @@ return new class extends Migration
     {
         Schema::create('patient_history', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('client_id')->constrained('clients');
-            $table->foreignId('treatment_id')->constrained('treatments');
             $table->text('observation');
             $table->date('date');
             $table->timestamps();
