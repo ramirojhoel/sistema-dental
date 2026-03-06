fdc64b64888725d2eae206e63b6fa4c6296f23de
ramirojhoel <ramirojhoel1998@gmail.com>
2026-03-05 20:40:15 -0400

    realize las rutas registradas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PatientController;
+use App\Http\Controllers\MedicalAppointmentController;
+use App\Http\Controllers\TreatmentController;
 
+// Ruta de inicio
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Pacientes
+Route::resource('patients', PatientController::class);
+
+// Citas
+Route::resource('appointments', MedicalAppointmentController::class);
+
+// Tratamientos
+Route::resource('treatments', TreatmentController::class);
