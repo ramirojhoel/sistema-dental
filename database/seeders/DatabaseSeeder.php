d016ed4725ded461f8b8ea29b40c01fe4ff53384
ramirojhoel <ramirojhoel1998@gmail.com>
2026-03-15 00:52:42 -0400

    feat: arreglo del frontend y env de nuevo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,27 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+       User::create([
+    'name'      => 'Admin',
+    'last_name' => 'Sistema',
+    'email'     => 'admin@dental.com',
+    'password'  => Hash::make('123456'),
+    'role'      => 'admin',
+]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+User::create([
+    'name'      => 'María',
+    'last_name' => 'Recepcionista',
+    'email'     => 'maria@dental.com',
+    'password'  => Hash::make('12345678'),
+    'role'      => 'receptionist',
+]);
     }
 }
